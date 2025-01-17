7cb72ee7542a8bcc5884b80e05f1318c1afbf3b8
mohamedklby <moha.klby333@gmail.com>
2025-01-17 11:04:05 +0100

    kljhj

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 
-    if number > 0:i
-        print("{number} is positive", format (number)); #si number est plus grand que zérmt on afiiche qu'il est positive.
+    if number > 0:
+        print("{} is positive" .format (number)) #si number est plus grand que zérmt on afiiche qu'il est positive.
 
     elif number == 0:
-        print("{number} is zero", format (number)); #si number est egal a zéro on affiche qu'il est nul.
+        print("{} is zero" .format (number)) #si number est egal a zéro on affiche qu'il est nul.
 
     else:
-        print("{number} is negative", format (number)); #sinon on affiche qu'il est négative.
+        print("{} is negative" .format (number)) #sinon on affiche qu'il est négative.
