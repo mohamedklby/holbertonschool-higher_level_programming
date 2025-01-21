d28a980e0e1c55182c20333406afc7f7ffb2d87b
mohamedklby <moha.klby333@gmail.com>
2025-01-21 15:45:10 +0100

    kljjkj

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-
-    my_string = "Best School"
-    resultat = supprimer_tous_les_caracteres(my_string)
-
-
+    return(my_string.translate({ord('C'): None, ord('c'): None}))
