32c4791e1bfb80208baae3d72609bd3fb0a4ca6c
mohamedklby <moha.klby333@gmail.com>
2025-01-22 14:41:18 +0100

    jhjgh

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,5 +10,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     print()
     return p
-
-
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -8,5 +8,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(answer))
     return answer
-
-
