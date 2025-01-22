deb1118d48b85fa9a268029b5df8ada4bfc9b0cf
mohamedklby <moha.klby333@gmail.com>
2025-01-22 11:38:30 +0100

    kjhj

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
 
- new_matrix = [[element ** 2 for element in row]for row in matrix]
-    return new_matrix
+def square_matrix_simple(matrix=[]):
+    
+    new_matrix = [[element ** 2 for element in row]for row in matrix]
+        return new_matrix
 
