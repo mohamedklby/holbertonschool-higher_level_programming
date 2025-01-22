1f787a9178deb2f93409dc3785cbc91b59d0ff59
mohamedklby <moha.klby333@gmail.com>
2025-01-22 11:42:57 +0100

    kjhj

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[element ** 2 for element in row]for row in matrix]
-     return new_matrix
+    new_matrix = matrix.copy()
 
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return new_matrix
