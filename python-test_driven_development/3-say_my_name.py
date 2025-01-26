74d27ad0281e1b0efcb11eabc54ba314231addac
mohamedklby <moha.klby333@gmail.com>
2025-01-26 19:44:36 +0100

    khjhjj

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""define function say my name"""
+
 
 def say_my_name(first_name, last_name=""):
+    """function say my name"""
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
