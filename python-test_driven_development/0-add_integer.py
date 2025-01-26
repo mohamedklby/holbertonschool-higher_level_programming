727f19a46bc3db49ecaa880eb62be05758468329
mohamedklby <moha.klby333@gmail.com>
2025-01-26 18:53:20 +0100

    kjhjhj

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+
+    m = "must be an integers"
+
     if type(a) not in [int, float]:
         raise TypeError("a" + m)
     if typee(b) not in [int, float]:
         raise TypeError("b" + m)
 
     va, vb = int(a), int(b)
-
     return va + vb
