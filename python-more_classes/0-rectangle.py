1f0ef53701def5af2b452a2ee90660acedb1b1b9
mohamedklby <moha.klby333@gmail.com>
2025-01-30 14:46:58 +0100

    jukA

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""define a empty rectangle"""
+"""this module contains one class rectangle"""
 
 
 class rectangle
 
 
-"""the rectangle classes is empty"""
+"""define the rectangle classes"""
 
 pass
